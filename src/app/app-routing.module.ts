05bb4bbc1f3241a1250db84a3ff8bc3f99d9d110
Yovani Pahuara <yovani.pahuara.1596@gmail.com>
2021-10-14 13:15:54 -0500

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './modules/core/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'login', component: LoginComponent },
+  // { path: 'restore', component: RestoreComponent },
+  // { path: 'change-password', component: ChangePasswordComponent },
+  // { path: 'get-code', component: GetCodeComponent },
+  {
+      path: '',
+      canActivate: [AuthGuard],
+      loadChildren: () => import('./modules/features/features.module').then(m => m.CoreModule)
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/features/features.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './features-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ]
+})
+export class CoreModule { }
